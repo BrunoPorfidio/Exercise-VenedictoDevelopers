6baafabf5048f235a78c2dd1c8ea3833de8bc0d6
BrunoPorfidio <thewarriours7@gmail.com>
2024-09-28 11:13:04 -0300

    Quitando dependencias en desuso.

--- src/main/java/com/microservice/venedicto/repository/UserRepository.java
+++ src/main/java/com/microservice/venedicto/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.microservice.venedicto.repository;
 import com.microservice.venedicto.model.User;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
